310de94785933c783fbc521bda6ea9d03be9c650
aanuj67950 <aanuj67950@gmail.com>
2020-01-21 13:03:51 +0530

    DataInfoIDActivity

---
+++ app/src/main/java/com/vikaspandey121/progection/DataInfo.java
@@ -0,0 +1,14 @@
+package com.vikaspandey121.progection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DataInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_info);
+    }
+}
---
+++ app/src/main/java/com/vikaspandey121/progection/DataInfoID.java
@@ -0,0 +1,14 @@
+package com.vikaspandey121.progection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DataInfoID extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_info_id);
+    }
+}
